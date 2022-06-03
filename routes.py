180f40754ede60b68eb66aff92af50c3af71afaf
AliNajafi1998 <najafi1998ali@gmail.com>
2022-06-03 15:57:28 +0300

    path for files are added!

--- routes.py
+++ routes.py
@@ -1,4 +1,9 @@
 PATHS = {
-    "DATA": "/DATA/users/alinajafi",
+    "DATA": "/DATA/users/alinajafi/documentRanking",
+    # TRAIN
+    "QREL_TRAIN": "/DATA/users/alinajafi/documentRanking/msmarco-doctrain-qrels.tsv",
+    "QUERIES_TRAIN": "/DATA/users/alinajafi/documentRanking/queries.doctrain.tsv",
+    "DOC_TRAIN": "/DATA/users/alinajafi/documentRanking/msmarco-docs.tsv"
+    # TEST
 
 }
